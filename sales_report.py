750ee18f82a2c440decb6b4ccae392ac6fbf4884
Kaitlin Rosen <kayejrosen@gmail.com>
2022-03-22 17:28:25 -0700

    Created Sales Dictionary

--- sales_report.py
+++ sales_report.py
@@ -2,9 +2,7 @@
 '''Anna Parker|1881.91|13
 name|total amount for order|total melons sold'''
 
-##corresponding lists of salespeople and their total melons sold. can be a dict
-salespeople = []
-melons_sold = []
+sales_records = {}
 
 f = open('sales-report.txt')
 ##iterates through the file line by line (\n to \n)
@@ -14,14 +12,8 @@ for line in f:
     salesperson = entries[0]##establishes variable for saleperson name
     melons = int(entries[2])##establishes variable for total melons sold
 
-    '''this can be handled by adding names as keys and melons sold as values'''
-    if salesperson in salespeople:##if the name is in our salesperson list
-        position = salespeople.index(salesperson)##establishes their index
-        melons_sold[position] += melons##adds total melons sold to their larger sales record      
-    else:
-        salespeople.append(salesperson)##if not already in the list, add them
-        melons_sold.append(melons)##and add their melon sales
+    sales_records[salesperson] = sales_records.get(salesperson, 0) + melons
 
-##prints the total sales per person
-for i in range(len(salespeople)):
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+
+for sale in sorted(sales_records):
+    print(f"{sale} sold {sales_records[sale]} melons.")
