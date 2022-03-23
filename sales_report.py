a569cb0c0e3888b18c0e7d28b8ae1049ab7f64c3
Kaitlin Rosen <kayejrosen@gmail.com>
2022-03-22 17:21:06 -0700

    Commented working code

--- sales_report.py
+++ sales_report.py
@@ -2,25 +2,26 @@
 '''Anna Parker|1881.91|13
 name|total amount for order|total melons sold'''
 
+##corresponding lists of salespeople and their total melons sold. can be a dict
 salespeople = []
 melons_sold = []
 
 f = open('sales-report.txt')
-
+##iterates through the file line by line (\n to \n)
 for line in f:
-    line = line.rstrip()
-    entries = line.split('|')
-    salesperson = entries[0]
-    melons = int(entries[2])
+    line = line.rstrip()##clears the \nbreaks
+    entries = line.split('|')##creates a list for each item in line
+    salesperson = entries[0]##establishes variable for saleperson name
+    melons = int(entries[2])##establishes variable for total melons sold
 
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
-        melons_sold[position] += melons
-        
+    '''this can be handled by adding names as keys and melons sold as values'''
+    if salesperson in salespeople:##if the name is in our salesperson list
+        position = salespeople.index(salesperson)##establishes their index
+        melons_sold[position] += melons##adds total melons sold to their larger sales record      
     else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
+        salespeople.append(salesperson)##if not already in the list, add them
+        melons_sold.append(melons)##and add their melon sales
 
-
+##prints the total sales per person
 for i in range(len(salespeople)):
     print(f'{salespeople[i]} sold {melons_sold[i]} melons')
